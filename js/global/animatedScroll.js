24c767b7e3ed99054a8550bfca3e487b1b9b6822
Juninho Alves <contato@juniordev.com.br>
2023-04-19 10:20:35 -0300

    Services home concluido e responsivo

---
+++ js/global/animatedScroll.js
@@ -0,0 +1,16 @@
+export default function initAnimatedScroll() {
+  const section = document.querySelectorAll("[data-scroll]");
+
+  function animateScroll() {
+    section.forEach((item) => {
+      const clientWindow = window.innerHeight * 0.7;
+      const sectionTop = item.getBoundingClientRect().top - clientWindow;
+
+      if (sectionTop < 0) {
+        item.classList.add("active-scroll");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", animateScroll);
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initMenuTop from "./global/menu-top.js";
 import initHamburguer from "./global/hamburguer.js";
+import initAnimatedScroll from "./global/animatedScroll.js";
 
 initMenuTop();
 initHamburguer();
+initAnimatedScroll();
